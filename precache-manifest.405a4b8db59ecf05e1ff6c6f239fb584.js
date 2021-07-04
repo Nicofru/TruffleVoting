3a9e2a45f03d8fc5cb66e075a5a2d11b9262d386
Nicofru <n.fruneau@gmail.com>
2021-07-05 01:16:41 +0200

    Updates

---
+++ precache-manifest.405a4b8db59ecf05e1ff6c6f239fb584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28096e235625024303c6afdb67b67c6",
+    "url": "/TruffleVoting/index.html"
+  },
+  {
+    "revision": "616ffae0224027d29124",
+    "url": "/TruffleVoting/static/css/2.86fbe410.chunk.css"
+  },
+  {
+    "revision": "4f780af833657c23dd80",
+    "url": "/TruffleVoting/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "616ffae0224027d29124",
+    "url": "/TruffleVoting/static/js/2.881b3cfe.chunk.js"
+  },
+  {
+    "revision": "4f780af833657c23dd80",
+    "url": "/TruffleVoting/static/js/main.efd24466.chunk.js"
+  },
+  {
+    "revision": "d341e726d1626b3a0ff0",
+    "url": "/TruffleVoting/static/js/runtime-main.26f0158b.js"
+  }
+]);
